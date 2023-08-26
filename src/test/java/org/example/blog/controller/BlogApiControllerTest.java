e3f9ef3c2101459d52eedee89121473f17f53c8a
nativegold <zxc6941@gmail.com>
2023-08-26 20:52:10 +0900

    [ADD] Added BlogApiControllerTest.java to test application

---
+++ src/test/java/org/example/blog/controller/BlogApiControllerTest.java
@@ -0,0 +1,14 @@
+package org.example.blog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class BlogApiControllerTest {
+    @Autowired
+    
+}
