5d6e2c0606a75d6b59d66a25780def2606b46f1d
nativegold <zxc6941@gmail.com>
2023-09-05 18:47:17 +0900

    [ADD] Added HTML files

--- src/main/java/org/example/blog/controller/BlogViewController.java
+++ src/main/java/org/example/blog/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package org.example.blog.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.blog.domain.Article;
 import org.example.blog.dto.ArticleListViewResponse;
+import org.example.blog.dto.ArticleViewResponse;
 import org.example.blog.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -21,5 +24,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticles(@PathVariable Long id,  Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
