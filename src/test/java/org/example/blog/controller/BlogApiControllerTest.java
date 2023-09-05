c67a1f3b2abbe923e42eaae464cdbfa45faaeac5
nativegold <zxc6941@gmail.com>
2023-09-05 14:27:40 +0900

    [ADD] Added HTML files

--- src/test/java/org/example/blog/controller/BlogApiControllerTest.java
+++ src/test/java/org/example/blog/controller/BlogApiControllerTest.java
@@ -3,6 +3,7 @@ package org.example.blog.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.blog.domain.Article;
 import org.example.blog.dto.AddArticleRequest;
+import org.example.blog.dto.UpdateArticleRequest;
 import org.example.blog.repository.BlogRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -124,5 +125,34 @@ class BlogApiControllerTest {
         List<Article> articles = blogRepository.findAll();
 
         assertThat(articles).isEmpty();
+    }
+
+    @DisplayName("updateArticle: 블로그 글 수정에 성공한다.")
+    @Test
+    public void updateArticle() throws Exception {
+        final String url = "/api/articles/{id}";
+        final String title = "title";
+        final String content = "content";
+
+        Article savedArticle = blogRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
+
+        final String newTitle = "new title";
+        final String newContent = "new content";
+
+        UpdateArticleRequest request = new UpdateArticleRequest(newTitle, newContent);
+
+        ResultActions result = mockMvc.perform(put(url, savedArticle.getId())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(request)));
+
+        result.andExpect(status().isOk());
+
+        Article article = blogRepository.findById(savedArticle.getId()).get();
+
+        assertThat(article.getTitle()).isEqualTo(newTitle);
+        assertThat(article.getContent()).isEqualTo(newContent);
     }
 }
